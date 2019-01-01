91c5493d581c2aaf2850c03235fec9dd03a87ede
Paulo Cunha <paulo_cunha1997@outlook.pt>
2019-01-01 21:44:01 -0100

    Continue to develop add book methods (add and validate) and form. Fix get book method

--- Final-Academic-Projecto-WS-Client/application/views/bookworms/getbook.php
+++ Final-Academic-Projecto-WS-Client/application/views/bookworms/getbook.php
@@ -1,7 +1,7 @@
 <div class="row">
      <div class="col-lg-12">
        <br />
-       <h3> Books List </h3>
+       <h3>Book List</h3>
        <h6><button><a href="http://localhost/ws/Filmes/index.php/book/newbook">Insert a new book</a></button>
        <button><a href="http://localhost/ws/Filmes/index.php/book/newRate">Rate book</a></button></h6>
        <br>
